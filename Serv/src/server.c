e82b66d3a7df8a988d17b9f4dc07901d681ef2e9
Theo Le Bonniec <you@example.com>
2020-05-15 18:09:10 +0200

    fix send message

--- Serv/src/server.c
+++ Serv/src/server.c
@@ -70,19 +70,3 @@ void teams_server(server_t *srv) {
         }
     }
 }
-
-
-void str_trim_lf(char *arr, int length) {
-    int i;
-    for (i = 0; i < length; i++) { // trim \n
-        if (arr[i] == '\n') {
-            arr[i] = '\0';
-            break;
-        }
-    }
-}
-
-void str_overwrite_stdout() {
-    printf("\r%s", "> ");
-    fflush(stdout);
-}
