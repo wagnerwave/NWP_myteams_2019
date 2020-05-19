54ff68d963b093f11a26cff6fcdb619615206814
Theo Le Bonniec <you@example.com>
2020-05-19 23:18:24 +0200

    fix client

--- Client/src/client.c
+++ Client/src/client.c
@@ -11,18 +11,44 @@
 #include <stdio.h>
 #include "client.h"
 
-void client_run(client_t *cli)
-{
-    char *buffer = malloc(sizeof(char) * 1024);
-    int n = 0;
 
-    if (buffer == NULL)
-        exit(84);
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void client_run(client_t *cli) {
+    int fd;
+    int result;
+    char msg[1024 + 1];
+    char kb_msg[1024 + 10];
+    fd_set readfds, testfds, clientfds;
+    fflush(stdout);
+
+    FD_ZERO(&clientfds);
+    FD_SET(cli->tcp_sock, &clientfds);
+    FD_SET(0, &clientfds);//stdin
+
     while (1) {
-        bzero(buffer, 1024);
-        fgets(buffer, 1024, stdin);
-        n = write(cli->tcp_sock, buffer, strlen(buffer));
-        if (n < 0)
-           printf("error writing");
+        testfds = clientfds;
+        select(FD_SETSIZE, &testfds, NULL, NULL, NULL);
+
+        for (fd = 0; fd < FD_SETSIZE; fd++) {
+            if (FD_ISSET(fd, &testfds)) {
+                if (fd == cli->tcp_sock) {
+                    result = read(cli->tcp_sock, msg, 1024);
+                    msg[result] = '\0';
+                    printf("CLIENT GET : %s\n", msg);
+
+                } else if (fd == 0) {
+                    fgets(kb_msg, 1024 + 1, stdin);
+                        sprintf(msg, "%s", kb_msg);
+                        write(cli->tcp_sock, msg, strlen(msg));
+                }
+            }
+        }
     }
+
 }
