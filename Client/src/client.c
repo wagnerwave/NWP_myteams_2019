e4dbd19c90b3332d68ad469860db5f79dd8c190c
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-05-20 00:21:59 +0200

    correction client avec select work, need to continue implementation

--- Client/src/client.c
+++ Client/src/client.c
@@ -5,50 +5,48 @@
 ** client for teams
 */
 
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "client.h"
 
+static void in_the_socket(int fd, fd_set *clientfd, int tcp_sock)
+{
+    int result = 0;
+    char *input = NULL;
+    char msg[1024];
 
-#include <sys/socket.h>
-#include <sys/types.h>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+    if (fd == tcp_sock) {
+        result = read(tcp_sock, msg, 1024);
+        msg[result] = '\0';
+        printf("CLIENT GET : %s\n", msg);
+        // interpretation de la reponse du serveur
+    } else if (fd == 0) {
+        input = get_next_line(0);
+        printf("i enter : %s\n", input);
+        if (input == NULL)
+            return;
+        dprintf(tcp_sock, "%s\n", input);
+    }
+}
 
 void client_run(client_t *cli) {
-    int fd;
-    int result;
-    char msg[1024 + 1];
-    char kb_msg[1024 + 10];
-    fd_set readfds, testfds, clientfds;
-    fflush(stdout);
+    fd_set readfd;
+    fd_set clientfd;
 
-    FD_ZERO(&clientfds);
-    FD_SET(cli->tcp_sock, &clientfds);
-    FD_SET(0, &clientfds);//stdin
-
+    FD_ZERO(&clientfd);
+    FD_SET(cli->tcp_sock, &clientfd);
+    FD_SET(0, &clientfd);
     while (1) {
-        testfds = clientfds;
-        select(FD_SETSIZE, &testfds, NULL, NULL, NULL);
-
-        for (fd = 0; fd < FD_SETSIZE; fd++) {
-            if (FD_ISSET(fd, &testfds)) {
-                if (fd == cli->tcp_sock) {
-                    result = read(cli->tcp_sock, msg, 1024);
-                    msg[result] = '\0';
-                    printf("CLIENT GET : %s\n", msg);
-
-                } else if (fd == 0) {
-                    fgets(kb_msg, 1024 + 1, stdin);
-                        sprintf(msg, "%s", kb_msg);
-                        write(cli->tcp_sock, msg, strlen(msg));
-                }
-            }
+        readfd = clientfd;
+        if (select(FD_SETSIZE, &readfd ,NULL, NULL, NULL) < 0)
+            exit(84);
+        for (int fd = 0; fd < FD_SETSIZE; fd++) {
+            if (FD_ISSET(fd, &readfd))
+                in_the_socket(fd, &clientfd, cli->tcp_sock);
         }
     }
-
 }
