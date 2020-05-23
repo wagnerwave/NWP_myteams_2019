18ebaa856da911761eb6db3346095205e573dc03
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-05-23 17:01:06 +0200

    creation de code rfc

--- Serv/src/connexion_cmd.c
+++ Serv/src/connexion_cmd.c
@@ -19,7 +19,7 @@ static void connect_client(client_t *cli, user_t *tmp)
     cli->user.username = strdup(tmp->username);
     server_event_user_loaded(uuid_to_str(cli->user.user_id),
     cli->user.username);
-    dprintf(cli->fd, "Client connect [%s:%s]\n",
+    dprintf(cli->fd, "001 Client connect [%s:%s]\n",
     uuid_to_str(cli->user.user_id), cli->user.username);
 }
 
@@ -29,7 +29,7 @@ static void create_client_user(client_t *cli, char *username)
     cli->user.connected = true;
     uuid_generate(cli->user.user_id);
     server_event_user_created(uuid_to_str(cli->user.user_id), username);
-    dprintf(cli->fd, "Client connect [%s:%s]\n",
+    dprintf(cli->fd, "001 Client connect [%s:%s]\n",
     uuid_to_str(cli->user.user_id), cli->user.username);
     write_user_to_db(uuid_to_str(cli->user.user_id), cli->user.username);
 }
@@ -55,8 +55,8 @@ void logout(client_t **cli, int nb, char **txt)
 {
     (void)txt;
     server_event_user_logged_out(uuid_to_str(cli[nb]->user.user_id));
-    dprintf(cli[nb]->fd, "Client logged out [%s:%s]\n",
-    cli[nb]->user.user_id, cli[nb]->user.username);
+    dprintf(cli[nb]->fd, "002 Client logged out [%s:%s]\n",
+    uuid_to_str(cli[nb]->user.user_id), cli[nb]->user.username);
     cli[nb]->user.connected = false;
     close(cli[nb]->fd);
     FD_CLR(cli[nb]->fd, cli[nb]->group_fd);
